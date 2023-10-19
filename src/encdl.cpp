6043f7f5af881549e072bf9a343f99434552096f
Väisänen, Petri <petri.vaisanen@nordicsemi.no>
2023-10-19 11:40:36 +0300

    Encoder done for tv0

--- src/encdl.cpp
+++ src/encdl.cpp
@@ -9,6 +9,7 @@
 #include <xtensor/xindex_view.hpp>
 #include <xtensor/xio.hpp>
 #include <xtensor/xmanipulation.hpp>
+#include <xtensor/xoperation.hpp>
 #include <xtensor/xtensor_forward.hpp>
 
 void encDl(params_s *params) {
@@ -26,7 +27,7 @@ void encDl(params_s *params) {
     in_file.open("./dl/tv0/crc_gen_m.txt");
     xt::xarray<int> crcGenVec = xt::ravel(xt::load_csv<short>(in_file));
     xt::xarray<int> crcGenMtx = xt::transpose(crcGenVec.reshape({params->P, params->K}));
-    std::cout << "crcGenVec:" << std::endl
+    std::cout << "crcGenMtx:" << std::endl
               << xt::print_options::line_width(160) << crcGenMtx << std::endl;
     in_file.close();
 
@@ -78,4 +79,44 @@ void encDl(params_s *params) {
     xt::filter(frozenBits, infoIntrl > 0) = intrlBits;
     std::cout << xt::print_options::line_width(160) << "frozenBits:" << std::endl
               << xt::transpose(frozenBits) << std::endl;
+
+    // Read encoder matrix
+    in_file.open("./dl/tv0/enc_gen_m.txt");
+    xt::xarray<int> encGenVec = xt::ravel(xt::load_csv<short>(in_file));
+    xt::xarray<int> encGenMtx = xt::transpose(encGenVec.reshape({params->N, params->N}));
+    std::cout << "encGenMtx:" << std::endl
+              << xt::print_options::line_width(160) << xt::print_options::edge_items(20)
+              << encGenMtx << std::endl;
+    in_file.close();
+
+    // Encoding
+    xt::xarray<int> encBits = xt::linalg::dot(frozenBits, encGenMtx);
+    encBits %= 2;
+    std::cout << xt::print_options::line_width(160) << "encBits:" << std::endl
+              << xt::transpose(encBits) << std::endl;
+
+    // Read rate matching pattern
+    in_file.open("./dl/tv0/rate_matching_pattern.txt");
+    xt::xarray<int> encIntrl = xt::ravel(xt::load_csv<int>(in_file));
+    std::cout << xt::print_options::line_width(160) << "encIntrl:" << std::endl
+              << xt::transpose(encIntrl) << std::endl;
+    in_file.close();
+
+    // Rate matching
+    xt::xarray<int> rmBits = xt::zeros<int>({params->E});
+    rmBits = xt::index_view(encBits, encIntrl);
+    std::cout << xt::print_options::line_width(160) << "rmBits:" << std::endl
+              << xt::transpose(rmBits) << std::endl;
+
+    // Read rate matched reference bits
+    in_file.open("./dl/tv0/rm_bits.txt");
+    xt::xarray<int> rmRefs = xt::ravel(xt::load_csv<int>(in_file));
+    std::cout << xt::print_options::line_width(160) << "rmRefs:" << std::endl
+              << xt::transpose(rmRefs) << std::endl;
+    in_file.close();
+
+    // Check results
+    xt::xarray<int> checkBits = xt::abs(rmRefs - rmBits);
+    int nDiffBits = xt::sum(checkBits)();
+    std::cout << "nDiffBits: " << nDiffBits << std::endl;
 }
